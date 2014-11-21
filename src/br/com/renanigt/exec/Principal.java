f47d24b278c95ffa086cae911621828c90298a0a
Renan Montenegro <renanigt@gmail.com>
2014-11-20 22:02:03 -0300

    Minor.

--- src/br/com/renanigt/exec/Principal.java
+++ src/br/com/renanigt/exec/Principal.java
@@ -11,9 +11,8 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class Principal {
 
 	public static void main(String[] args) {
-			Workbook workbook;
 			try {
-				workbook = WorkbookFactory.create(new File("/home/renan/Desktop/curva1.xlsx"));
+				Workbook workbook = WorkbookFactory.create(new File("/home/renan/Desktop/curva1.xlsx"));
 				Row row = workbook.getSheetAt(0).getRow(9);
 				System.out.println(row.getCell(4).getNumericCellValue());
 			} catch (InvalidFormatException e) {
